b53fd47f799d24168154fd9df0d345b495e87178
MaciejZab <112476759+MaciejZab@users.noreply.github.com>
2024-02-12 08:01:06 +0100

    idk

---
+++ node/src/index.ts
@@ -0,0 +1,17 @@
+// import express from "express";
+// // import pg from "pg";
+// // import cors from "cors";
+
+// // import { corsConfig } from "./config/cors.cjs";
+// import { serverConfig } from "./config/server.cjs";
+// // import { databaseConfig } from "./config/db.cjs";
+
+// const app = express();
+
+// app.get("/hello", (req, res) => {
+//   res.send("Hello, world!"); // Simple text response
+// });
+
+// // app.use(cors(corsConfig));
+
+// app.listen(serverConfig.port);
--- node/src/interfaces/config/PortConfig.cts
+++ node/src/interfaces/config/PortConfig.cts
@@ -1,4 +1,3 @@
-// PortConfig.ts
 interface PortConfig {
   port: number;
 }
